f13052cd7d761c1a4f7b9b8138a63606b2a1c795
Kehinde-Agboola <kehindeagboola52@gmail.com>
2025-06-17 04:33:26 +0100

    added reduce.js

--- reduce.js
+++ reduce.js
@@ -1,1 +1,23 @@
-// my name is kehinde
+
+const reduce = (nums, fn, init) => {
+    let val = init;
+    for (let i = 0; i < nums.length; i++){
+        val = fn(val, nums[i], i);
+    }
+    return val;
+}
+// const result = reduce([1, 2, 3])
+// console.log(result);
+
+// const reduce = (nums, cal, init) => { 
+//     let accum = init;
+//     if(nums.length === 0) {
+//         return accum;
+//     }
+//     for (let i = 0; i < nums.length; i++) {
+//         accum = cal(accum, nums[i], i);
+//     }
+//     return accum;
+// }
+const result = reduce([1, 2, 3], (acc, num) => acc + num, 0);
+console.log(result); // Output: 6
